93fd9755000ca813593574da9a886c6eb0e475c0
Juan Camilo Ortiz González <67029750+juancortizgonz@users.noreply.github.com>
2023-02-01 13:35:24 -0500

    init.sql actualizado con nuevo esquema

--- database/init.sql
+++ database/init.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE api
+CREATE DATABASE mande
     WITH 
     OWNER = postgres
     ENCODING = 'UTF8'
@@ -8,107 +8,137 @@ CREATE DATABASE api
     CONNECTION LIMIT = -1
     TEMPLATE template0;
 
-\c api
+\c mande
 
-CREATE TABLE public.usuario
+CREATE TABLE IF NOT EXISTS public."Usuario"
 (
-    id_usuario serial,
-    nombre_usuario character varying(60) NOT NULL,
-    email_usuario character varying(254) NOT NULL,
-    password_usuario character varying NOT NULL,
-    direccion_usuario character varying NOT NULL,
-    path_doc text NOT NULL,
-    telefono_usuario text,
+    id_usuario integer NOT NULL DEFAULT 'nextval('"Usuario_id_usuario_seq"'::regclass)',
+    nombre character varying COLLATE pg_catalog."default" NOT NULL,
+    email character varying COLLATE pg_catalog."default" NOT NULL,
+    telefono character varying COLLATE pg_catalog."default" NOT NULL,
+    psw character varying COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT usuario_pkey PRIMARY KEY (id_usuario)
-);
+)
 
-CREATE TABLE public.cliente
+TABLESPACE pg_default;
+
+CREATE TABLE IF NOT EXISTS public."Cliente"
 (
-    id_cliente serial,
+    id_cliente integer NOT NULL DEFAULT 'nextval('"Cliente_id_cliente_seq"'::regclass)',
+    recibo character varying COLLATE pg_catalog."default" NOT NULL,
+    lat_dir character varying COLLATE pg_catalog."default" NOT NULL,
+    lon_dir character varying COLLATE pg_catalog."default" NOT NULL,
     id_usuario integer,
     CONSTRAINT cliente_pkey PRIMARY KEY (id_cliente),
     CONSTRAINT cliente_usuario_fkey FOREIGN KEY (id_usuario)
-        REFERENCES public.usuario (id_usuario) MATCH SIMPLE
+        REFERENCES public."Usuario" (id_usuario) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE
         NOT VALID
-);
+)
 
-CREATE TABLE public.trabajador
+TABLESPACE pg_default;
+
+CREATE TABLE IF NOT EXISTS public."Trabajador"
 (
-    id_trabajador serial,
-    id_usuario integer,
-    foto text NOT NULL,
+    id_trabajador integer NOT NULL DEFAULT 'nextval('"Trabajador_id_trabajador_seq"'::regclass)',
+    foto character varying COLLATE pg_catalog."default" NOT NULL,
+    cedula character varying COLLATE pg_catalog."default" NOT NULL,
+    soporte_cedula character varying COLLATE pg_catalog."default" NOT NULL,
+    lat_dir character varying COLLATE pg_catalog."default" NOT NULL,
+    lon_dir character varying COLLATE pg_catalog."default" NOT NULL,
+    id_usuario integer NOT NULL,
     CONSTRAINT trabajador_pkey PRIMARY KEY (id_trabajador),
     CONSTRAINT trabajador_usuario_fkey FOREIGN KEY (id_usuario)
-        REFERENCES public.usuario (id_usuario) MATCH SIMPLE
+        REFERENCES public."Usuario" (id_usuario) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE
-        NOT VALID
-);
+)
 
-CREATE TABLE public.labor
+TABLESPACE pg_default;
+
+CREATE TABLE IF NOT EXISTS public."Labor"
 (
-    id_labor serial,
-    nombre_labor character varying(450) NOT NULL,
-    descripcion_labor character varying,
-    unidad_labor character varying,
+    id_labor integer NOT NULL DEFAULT 'nextval('"Labor_id_labor_seq"'::regclass)',
+    nombre character varying COLLATE pg_catalog."default" NOT NULL,
+    descripcion character varying COLLATE pg_catalog."default" NOT NULL,
+    unidad character varying COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT labor_pkey PRIMARY KEY (id_labor)
-);
+)
 
-CREATE TABLE public.tarifa
+TABLESPACE pg_default;
+
+CREATE TABLE IF NOT EXISTS public."Tarifa"
 (
-    id_tarifa serial,
-    tarifa integer NOT NULL,
+    id_tarifa integer NOT NULL DEFAULT 'nextval('"Tarifa_id_tarifa_seq"'::regclass)',
+    precio integer NOT NULL,
     id_trabajador integer,
     id_labor integer,
     CONSTRAINT tarifa_pkey PRIMARY KEY (id_tarifa),
-    CONSTRAINT tarifa_trabajador_fkey FOREIGN KEY (id_trabajador)
-        REFERENCES public.trabajador (id_trabajador) MATCH SIMPLE
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-        NOT VALID,
     CONSTRAINT tarifa_labor_fkey FOREIGN KEY (id_labor)
-        REFERENCES public.labor (id_labor) MATCH SIMPLE
+        REFERENCES public."Labor" (id_labor) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT tarifa_trabajador_fkey FOREIGN KEY (id_trabajador)
+        REFERENCES public."Trabajador" (id_trabajador) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE
-        NOT VALID
-);
+)
 
-CREATE TABLE IF NOT EXISTS public.servicio
-(
-    id_servicio integer NOT NULL DEFAULT 'nextval('servicio_id_servicio_seq'::regclass)',
-    costo_servicio integer,
-    calificacion_servicio integer,
-    unidades_servicio character varying COLLATE pg_catalog."default",
-    fecha_servicio date,
-    estado_servicio boolean,
-    id_oferta integer,
-    CONSTRAINT servicio_pkey PRIMARY KEY (id_servicio),
-    CONSTRAINT servicio_oferta_fkey FOREIGN KEY (id_oferta)
-        REFERENCES public.oferta (id_oferta) MATCH SIMPLE
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-        NOT VALID
-);
+TABLESPACE pg_default;
 
-CREATE TABLE IF NOT EXISTS public.oferta
+CREATE TABLE IF NOT EXISTS public."Oferta"
 (
-    id_oferta integer NOT NULL DEFAULT 'nextval('oferta_id_oferta_seq'::regclass)',
+    id_oferta integer NOT NULL DEFAULT 'nextval('"Oferta_id_oferta_seq"'::regclass)',
     id_trabajador integer,
-    id_tarifa integer,
     id_labor integer,
+    id_tarifa integer,
     CONSTRAINT oferta_pkey PRIMARY KEY (id_oferta),
     CONSTRAINT oferta_labor_fkey FOREIGN KEY (id_labor)
-        REFERENCES public.labor (id_labor) MATCH SIMPLE
+        REFERENCES public."Labor" (id_labor) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE,
     CONSTRAINT oferta_tarifa_fkey FOREIGN KEY (id_tarifa)
-        REFERENCES public.tarifa (id_tarifa) MATCH SIMPLE
+        REFERENCES public."Tarifa" (id_tarifa) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE,
     CONSTRAINT oferta_trabajador_fkey FOREIGN KEY (id_trabajador)
-        REFERENCES public.trabajador (id_trabajador) MATCH SIMPLE
+        REFERENCES public."Trabajador" (id_trabajador) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE
-);
+)
+
+TABLESPACE pg_default;
+
+CREATE TABLE IF NOT EXISTS public."Servicio"
+(
+    id_servicio integer NOT NULL DEFAULT 'nextval('"Servicio_id_servicio_seq"'::regclass)',
+    costo character varying COLLATE pg_catalog."default" NOT NULL,
+    calificacion integer NOT NULL,
+    fecha date NOT NULL,
+    pagada boolean NOT NULL DEFAULT 'false',
+    id_oferta integer,
+    CONSTRAINT servicio_pkey PRIMARY KEY (id_servicio),
+    CONSTRAINT servicio_oferta_fkey FOREIGN KEY (id_oferta)
+        REFERENCES public."Oferta" (id_oferta) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+)
+
+TABLESPACE pg_default;
+
+CREATE TABLE IF NOT EXISTS public."Metodo_Pago"
+(
+    id_metodo_pago integer NOT NULL DEFAULT 'nextval('"Metodo_Pago_id_metodo_pago_seq"'::regclass)',
+    tipo_tarjeta integer NOT NULL,
+    numero_tarjeta character varying COLLATE pg_catalog."default" NOT NULL,
+    id_cliente integer,
+    CONSTRAINT metodo_pago_pkey PRIMARY KEY (id_metodo_pago),
+    CONSTRAINT metodo_pago_cliente_fkey FOREIGN KEY (id_cliente)
+        REFERENCES public."Cliente" (id_cliente) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+        NOT VALID
+)
+
+TABLESPACE pg_default;
